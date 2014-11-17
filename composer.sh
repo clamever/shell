5815a7011384551fdc205ab3a12ec1c1d5096596
clamever <1095841676@qq.com>
2014-11-17 22:22:19 +0800

    用来执行laravel项目的composer autoload 命令
    用来执行laravel项目的composer autoload 命令

---
+++ composer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#cd /mnt/hgfs/dameiti/ && /usr/local/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/aggregate && /usr/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/content && /usr/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/crontab && /usr/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/app && /usr/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/user && /usr/bin/composer $1
+cd /mnt/hgfs/dameiti_zhangqiming/workbench/cdv/account && /usr/bin/composer $1
